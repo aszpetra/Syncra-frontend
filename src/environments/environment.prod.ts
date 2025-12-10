e075bff74835ae1172d7717918af799f5c4fea42
aszpetra <petragabriella2002@gmail.com>
2025-12-10 11:38:17 +0100

    update backend url after deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   googleClientId: '14761851350-hi51etuaovspnu3ut64u66c4j099bnl4.apps.googleusercontent.com',
-  apiBaseUrl: 'http://localhost:3000',
-  redirectUri: 'http://localhost:3000/auth'
+  apiBaseUrl: 'https://syncra-backend-o7g9.onrender.com',
+  redirectUri: 'https://syncra-backend-o7g9.onrender.com/auth'
 }
